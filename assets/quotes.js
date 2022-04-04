117b7ab3f9908512dce9348801164c1a262aed40
Matt Haynes <74832450+MattHaynesDev@users.noreply.github.com>
2022-04-04 19:02:20 +0100

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -16,27 +16,27 @@ const quotes = [
     "author": "John Grogan"
   },
   {
-    "text": "The problem with cats is that they get the same exact look whether they see a moth or an ax-murderer.",
-    "author": "Paula Poundstone"
+    "text": "You can usually tell that a man is good if he has a dog who loves him.",
+    "author": "W. Bruce Cameron"
   },
   {
-    "text": "Cats can work out mathematically the exact place to sit that will cause the most inconvenience.",
-    "author": "Pam Brown"
+    "text": "What do dogs do on their day off? Can't lie around—that's their job.",
+    "author": "George Carlin"
   },
   {
-    "text": "A cat determined not to be found can fold itself up like a pocket handkerchief if it wants to.",
-    "author": "Louis J. Camuti"
+    "text": "If there are no dogs in heaven, then when I die I want to go where they went.",
+    "author": "Will Rogers"
   },
   {
-    "text": "When I play with my cat, how do I know that she is not passing time with me rather than I with her?",
-    "author": "Montaigne"
+    "text": "There is no faith which has never yet been broken, except that of a truly faithful dog.",
+    "author": "Konrad Lorenz"
   },
   {
-    "text": "I would like to see anyone, prophet, king or God, convince a thousand cats to do the same thing at the same time.",
-    "author": "Neil Gaiman"
+    "text": "Dogs’ lives are too short. Their only fault, really.",
+    "author": "Agnes Sligh Turnbull"
   },
   {
-    "text": "Cats do care. For example they know instinctively what time we have to be at work in the morning and they wake us up twenty minutes before the alarm goes off.",
-    "author": "Michael Nelson"
+    "text": "No matter how little money and how few possessions you own, having a dog makes you rich.",
+    "author": "Louis Sabin"
   }
 ]
