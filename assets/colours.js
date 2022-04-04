8070a177165a3cc44d2bc831355a8c3eeba5e15c
Matt Haynes <74832450+MattHaynesDev@users.noreply.github.com>
2022-04-04 19:12:53 +0100

    Update colours.js

--- assets/colours.js
+++ assets/colours.js
@@ -1,12 +1,12 @@
 const colours = [
-  "C9F0D6",
-  "D5E0AA",
-  "C2C9EE",
-  "B8DDCE",
-  "F0E6C2",
-  "FED9CC",
-  "ECC2D1",
-  "CDBADB",
-  "B6D1E5",
-  "BAEDF2"
+  "666547",
+  "fb2e01",
+  "6fcb9f",
+  "ffe28a",
+  "fffeb3",
+  "fe0000",
+  "fdfe02",
+  "0bff01",
+  "011efe",
+  "fe00f6"
 ]
