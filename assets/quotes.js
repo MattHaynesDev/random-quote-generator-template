2b8da92d8b686ec7ea317f474c9a2651348dd0b7
Matt Haynes <74832450+MattHaynesDev@users.noreply.github.com>
2022-04-04 18:57:37 +0100

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,19 +1,19 @@
 const quotes = [
   {
-    "text": "I read that when cats are cuddling and kneading you, and you think it’s cute, they’re really just checking your vitals for weak spots.",
-    "author": "Kandyse McClure"
+    "text": "I want to work like a dog, doing what I was born to do with joy and purpose. I want to play like a dog, with total, jolly abandon.",
+    "author": "Oprah Winfrey"
   },
   {
-    "text": "The way to get on with a cat is to treat it as an equal – or even better, as the superior it knows itself to be.",
-    "author": "Elizabeth Peters"
+    "text": "The dog is a gentleman; I hope to go to his heaven not man's.",
+    "author": "Mark Twain"
   },
   {
-    "text": "Cats have a scam going – you buy the food, they eat the food, they go away; that’s the deal.",
-    "author": "​​Eddie Izzard"
+    "text": "If you don't own a dog, at least one, there may not necessarily be anything wrong with you, but there may be something wrong with your life.",
+    "author": "Roger Caras"
   },
   {
-    "text": "Everything I know I learned from my cat: When you’re hungry, eat. When you’re tired, nap in a sunbeam. When you go to the vet’s, pee on your owner.",
-    "author": "Gary Smith"
+    "text": "Such short little lives our pets have to spend with us, and they spend most of it waiting for us to come home each day.",
+    "author": "John Grogan"
   },
   {
     "text": "The problem with cats is that they get the same exact look whether they see a moth or an ax-murderer.",
